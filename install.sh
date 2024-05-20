945d68d8689a56db39fd7ea9347a3dabeb9a613b
bob <zzzooozzz836@gmail.com>
2024-05-20 22:15:10 +0545

    up

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
-sudo pacman -S man 
+sudo pacman -S man
 read -p "Continue? (y/n) " response
+
+if test $response = "y"
+  # Continue with the installation
+  echo "Installing..."
+else
+  # Cancel the installation
+  echo "Installation cancelled."
+  exit 1
+end
