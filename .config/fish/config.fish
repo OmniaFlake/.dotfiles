e93ab98aef5de7fece4fc30718292b9b3594347d
bob <zzzooozzz836@gmail.com>
2024-05-22 23:16:37 +0545

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
-    zoxide init fish | source # Commands to run in interactive sessions can go here
+    zoxide init fish | source
+    alias githubkey "cat ~/key | wl-copy"
+    alias source-fish "source ~/.config/fish/config.fish"
+    alias haki-nvim "rm -rf ~/.config/nvim && rm -rf ~/.local/share/nvim"   
+    # Commands to run in interactive sessions can go here
 end
