93b95c1db987eaf7ceeed4f60769ff081c95af06
bob <zzzooozzz836@gmail.com>
2024-05-25 11:38:01 +0545

    lol

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,9 @@
 if status is-interactive
     zoxide init fish | source
     alias githubkey "cat ~/key | wl-copy"
+    alias shello "cd ~/projects/rust/shell && cargo run "
     alias source-fish "source ~/.config/fish/config.fish"
-    alias haki-nvim "rm -rf ~/.config/nvim && rm -rf ~/.local/share/nvim"   
+    alias haki-nvim "rm -rf ~/.config/nvim && rm -rf ~/.local/share/nvim"  
+    alias nvim-config "git clone https://github.com/TheNewLearner1/stupidnvim ~/.config/nvim"
     # Commands to run in interactive sessions can go here
 end
