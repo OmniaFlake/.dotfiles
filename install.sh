c8fc34d293f9e0be3747c3c8adfbdfa90426c0b9
zzzooozzz836@gmail.com <zzzooozzz836@gmail.com>
2024-06-18 21:12:49 +0545

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-sudo pacman -S man stow wl-clipboard fish hyprpaper fastfetch ripgrep neovim thunar rust kitty tmux zellij brightnessctl wireplumber openssh nerd-fonts python3 wofi go zoxide nodejs 
+sudo pacman -S man stow wl-clipboard waybar fish hyprpaper qbittorent fastfetch ripgrep neovim thunar rust kitty tmux zellij brightnessctl wireplumber openssh nerd-fonts python3 wofi go zoxide nodejs npm  
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+yay -S firefox
 chsh
 
 
