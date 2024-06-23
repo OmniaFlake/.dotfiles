52505e3c718add76a7bf0948d78d54550ff602a6
zzzz <zzzooozzz836@gmail.com>
2024-06-23 21:48:13 +0545

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
-sudo pacman -S man stow wl-clipboard waybar fish hyprpaper qbittorent fastfetch ripgrep neovim thunar rust kitty tmux zellij brightnessctl wireplumber openssh nerd-fonts python3 wofi go zoxide nodejs npm  
+
+
+
+sudo pacman -S man stow wl-clipboard waybar fish hyprpaper qbittorrent fastfetch ripgrep neovim thunar rust kitty tmux zellij brightnessctl wireplumber openssh nerd-fonts python3 wofi go zoxide nodejs npm gnome-disk-utility 
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 yay -S firefox
+cd ~/.dotfiles
+rm -rf ~/.config
+stow .
 chsh
-
+sudo reboot now
 
