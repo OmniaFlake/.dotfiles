8e0e62c44b8181bc92545970778fd85d447f3970
zzzooozzz836@gmail.com <zzzooozzz836@gmail.com>
2024-08-06 22:44:59 +0545

    added git config --global

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 
 
 
-sudo pacman -S man stow wl-clipboard yazi fzf waybar fish hyprpaper qbittorrent fastfetch ripgrep neovim thunar rust kitty tmux zellij brightnessctl wireplumber openssh nerd-fonts yazi python3 zig wofi go zoxide nodejs npm gnome-disk-utility 
+sudo pacman -S man stow wl-clipboard yazi fzf waybar fish hyprpaper qbittorrent fastfetch ripgrep neovim thunar rust kitty tmux zellij brightnessctl wireplumber openssh nerd-fonts yazi python3 zig wofi go zoxide nodejs npm gnome-disk-utility
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -11,4 +11,8 @@ cd ~/.dotfiles
 rm -rf ~/.config
 stow .
 chsh
+git config --global user.email "zzzooozzz836@gmail.com"
+git config --global user.name "omnia"
+
+sudo pacman -Rs dolphin
 sudo reboot now
